ccb60a04889f4d74b8c2f262b6fcc3a52955c711
William Tran <tranwilliam129@tamu.edu>
2026-09-14 13:19:50 -0500

    Revert "Merge pull request #12 from tranwilliam129-tamu/book-collection-4-associations"
    This reverts commit 7663e4908376299edc9070cc4446a97732c0dce7, reversing
    changes made to 6e08aeb2cfc54e64f90f3a1e9f917213870c0386.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,3 @@
 class Book < ApplicationRecord
-  has_many :users, through: :user_books
-  has_many :user_books
-
   validates :title, presence: true
 end
--- app/models/user.rb
+++
@@ -1,4 +0,0 @@
-class User < ApplicationRecord
-  has_many :books, through: :user_books
-  has_many :user_books
-end
